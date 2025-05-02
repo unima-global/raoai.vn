986407e74f3c4bad2427a10eacfacd074c8e2d2d
Truong Van Phap <hotruongphuhoa@gmail.com>
2025-05-03 01:18:33 +0200

    🔥 Xoá toàn bộ logic client khỏi layout.tsx để fix lỗi RSC

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import NavBar from '../components/NavBar'; // đường dẫn tương đối
+import NavBar from '../components/NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
