e3c8efc0c3f95e2c98b1d8eaaec910191cad91a3
Truong Van Phap <hotruongphuhoa@gmail.com>
2025-05-02 20:20:48 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from '../../lib/supabase'
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
