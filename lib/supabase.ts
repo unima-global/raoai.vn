fb3ef3f9efb024d535893fc4da0bda00e9f5c61c
Truong Van Phap <hotruongphuhoa@gmail.com>
2025-05-02 20:32:17 +0200

    📦 Cài @supabase/supabase-js và sửa lại supabase.ts đúng chuẩn

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import { createClient } from '@supabase/supabase-js'
-=======
-
-import { supabase } from '../../lib/supabase'
->>>>>>> e3c8efc0c3f95e2c98b1d8eaaec910191cad91a3
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
