15aa99c7dbb52f4401b9cd41c052d565e814a8ac
Truong Van Phap <hotruongphuhoa@gmail.com>
2025-05-07 02:02:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import Navbar from '../components/NavBar'
+import Navbar from '../components/Navbar'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
