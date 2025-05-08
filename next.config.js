c634af199a33628fed5628b5c91c2ec3826e61ec
Truong Van Phap <hotruongphuhoa@gmail.com>
2025-05-08 19:16:15 +0200

    Sửa next.config.js chuẩn Next.js 14, xoá matcher

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {}, // bỏ 'serverActions: false' nếu có lỗi
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  reactStrictMode: true,
+  images: {
+    domains: ['i.pravatar.cc', 'ndcbkuxwgninbvqvmqnei.supabase.co'],
+  },
 };
 
 module.exports = nextConfig;
