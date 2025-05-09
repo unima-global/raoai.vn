24b39884ecda1bc08ff13df4b9f56321f8d94264
Truong Van Phap <hotruongphuhoa@gmail.com>
2025-05-09 05:13:19 +0200

    Xoá SupabaseProvider.tsx còn dùng auth-helpers-react

--- components/SupabaseProvider.tsx
+++ components/SupabaseProvider.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 
 export default function SupabaseProvider({ children }: { children: React.ReactNode }) {
